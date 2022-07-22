f78f6912292eaf371ba6365570408d0272036bd2
Maulana Adam Sahid <masadamsahid@gmail.com>
2022-07-22 13:41:38 +0700

    add `Logo Icon`

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from "styled-components";
 
+import LogoImg from '../img/logo.png';
+
 const Container = styled.div`
   flex: 1;
   background-color: #202020;
@@ -12,10 +14,28 @@ const Wrapper = styled.div`
   padding: 18px 26px;
 `;
 
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  font-weight: bold;
+  margin-bottom: 25px;
+  font-size: 14px;
+`;
+
+const Img = styled.img`
+  height: 25px;
+`
+
 const Menu = () => {
   return (
     <Container>
-      <Wrapper>items</Wrapper>
+      <Wrapper>
+        <Logo>
+          <Img src={LogoImg}/>
+          MasadamTube
+        </Logo>
+      </Wrapper>
     </Container>
   );
 };
