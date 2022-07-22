e89b87f1e4daec9fd11e558beeecbd739468802b
Maulana Adam Sahid <masadamsahid@gmail.com>
2022-07-22 15:15:45 +0700

    add sidebar menu `items`

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 import styled from "styled-components";
+import {
+  Home,
+  SportsBasketballOutlined,
+  VideoLibraryOutlined,
+  SubscriptionsOutlined,
+  ExploreOutlined,
+  LibraryMusicOutlined,
+  HistoryOutlined,
+  MovieOutlined,
+  NewspaperOutlined,
+  LiveTvOutlined,
+  SettingsOutlined,
+  ReportOutlined,
+  HelpOutline,
+  SportsEsportsOutlined, SettingsBrightnessOutlined,
+} from '@mui/icons-material'
 
 import LogoImg from '../img/logo.png';
 
@@ -23,8 +39,22 @@ const Logo = styled.div`
   font-size: 14px;
 `;
 
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  cursor: pointer;
+  padding: 7.5px 0;
+  font-size: 12px;
+`
+
 const Img = styled.img`
   height: 25px;
+`
+
+const Hr = styled.hr`
+  margin: 15px 0;
+  border: .5px solid #373737;
 `
 
 const Menu = () => {
@@ -35,6 +65,69 @@ const Menu = () => {
           <Img src={LogoImg}/>
           MasadamTube
         </Logo>
+        <Item>
+          <Home/>
+          Home
+        </Item>
+        <Item>
+          <ExploreOutlined/>
+          Explore
+        </Item>
+        <Item>
+          <SubscriptionsOutlined/>
+          Subscriptions
+        </Item>
+        <Hr/>
+        <Item>
+          <VideoLibraryOutlined/>
+          Library
+        </Item>
+        <Item>
+          <HistoryOutlined/>
+          History
+        </Item>
+        <Hr/>
+        <Item>
+          <LibraryMusicOutlined/>
+          Music
+        </Item>
+        <Item>
+          <SportsBasketballOutlined/>
+          Sports
+        </Item>
+        <Item>
+          <SportsEsportsOutlined/>
+          Gaming
+        </Item>
+        <Item>
+          <MovieOutlined/>
+          Movies
+        </Item>
+        <Item>
+          <NewspaperOutlined/>
+          News
+        </Item>
+        <Item>
+          <LiveTvOutlined/>
+          Live
+        </Item>
+        <Hr/>
+        <Item>
+          <SettingsOutlined/>
+          Settings
+        </Item>
+        <Item>
+          <ReportOutlined/>
+          Report
+        </Item>
+        <Item>
+          <HelpOutline/>
+          Help
+        </Item>
+        <Item>
+          <SettingsBrightnessOutlined/>
+          Light Mode
+        </Item>
       </Wrapper>
     </Container>
   );
