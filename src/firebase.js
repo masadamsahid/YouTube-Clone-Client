385e3cc873726f40b1eae4694e51c8fe6e751662
Maulana Adam Sahid <masadamsahid@gmail.com>
2022-07-27 03:23:29 +0700

    make google auth provider set custom parameter in a single line

--- src/firebase.js
+++ src/firebase.js
@@ -18,8 +18,6 @@ export const auth = getAuth();
 
 export const provider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
-  prompt: 'select_account'
-});
+provider.setCustomParameters({prompt: 'select_account'});
 
 export default app;
